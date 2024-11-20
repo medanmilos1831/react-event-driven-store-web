af71402cdf67acf628fb761c2f74416aef4e59e9
medanmilos1831 <medanmilos1831@gmail.com>
2024-11-20 08:56:08 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,12 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: __dirname,
-  cacheDir: './node_modules/.vite/react-event-driven-store-web',
+  plugins: [react()],
+  base: '/react-event-driven-store-web/',
+
   server: {
-    port: 4200,
-    host: 'localhost',
-  },
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  base: '/react-event-driven-store-web',
-  build: {
-    outDir: './dist/react-event-driven-store-web',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './coverage/react-event-driven-store-web',
-      provider: 'v8',
-    },
+    port: 4202,
   },
 });
